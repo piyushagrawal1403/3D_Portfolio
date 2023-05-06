3bf210e6cfa85460d2e6e04efa060674836f2bbb
Neelesh Thakur <thakurne@vmware.com>
2023-05-06 17:09:25 +0530

    added resume public link form drive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,6 +31,14 @@ const Navbar = () => {
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
+          <li
+              key="resume-download"
+              className={`text-secondary hover:text-white text[-18px] font-medium cursor-pointer`}
+            >
+              <a download href={`https://drive.google.com/file/d/1Ls9aSVyVcA_Nfg6FboyNNG5iqOqwcO2b/view?usp=share_link`} target="_blank">
+                Resume
+              </a>
+            </li>
           {navLinks.map((link) => (
             <li
               key={link.id}
@@ -52,8 +60,7 @@ const Navbar = () => {
           />
           <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient 
           absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
-            <ul className="list-none flex justify-end items-start 
-            flex-col gap-4">
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
               {navLinks.map((link) => (
                 <li
                   key={link.id}
