a4a735f6a2a3e29972659f000d7e0daf9b196bda
Neelesh Thakur <thakurne@vmware.com>
2023-05-06 11:03:49 +0530

    build feedback section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -56,7 +56,7 @@ const ProjectCard = ({index, name, description, tags, image, source_code_link })
 const Works = () => {
   return (
     <>
-      <motion.div variants={textVariant}>
+      <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText }>My work</p>
         <h2 className={styles.sectionHeadText}>Projects.</h2>
       </motion.div>
